4c1819264624296a54794a47915de7489b6b22d3
Stijn Mertens <stijn.mertens@expertum.net>
2022-12-28 19:03:16 +0100

    init: keymaps

---
+++ .config/nvim/lua/melxaco/core/keymaps.lua
@@ -0,0 +1,16 @@
+vim.g.mapleader=" " -- default in vim is backspace
+
+local keymap = vim.keymap
+
+--keymap.set("n","j","jzz")
+--keymap.set("n","k","kzz")
+
+-- nvim-tree
+keymap.set("n","<leader>e",":NvimTreeToggle<CR>")
+
+-- telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags

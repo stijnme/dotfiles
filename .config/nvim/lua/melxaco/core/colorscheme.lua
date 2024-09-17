0efd8aec9cecbf7c7191ca1c7b60b7a029fec3bb
Stijn Mertens <stijn.mertens@expertum.net>
2024-09-17 21:38:02 +0200

    feat: added colorscheme setup

--- .config/nvim/lua/melxaco/core/colorscheme.lua
+++ .config/nvim/lua/melxaco/core/colorscheme.lua
@@ -1,5 +1,14 @@
-local status, _ = pcall(vim.cmd, "colorscheme nightfox")
-if not status then
-  print ("Colorscheme not found!")
-  return
-end
+print("[D] Colorscheme loading (melxaco.core.colorscheme)")
+
+return {
+  -- the colorscheme should be available when starting Neovim
+  {
+    "EdenEast/nightfox.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      vim.cmd("colorscheme nightfox")
+    end,
+  }
+}

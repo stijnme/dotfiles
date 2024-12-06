479c7fe2c2dda492afcb73d7b8c1192477415501
Stijn Mertens <stijn.mertens@expertum.net>
2024-12-06 17:19:41 +0100

    feat: changed folder arrow icons in nvim-tree

--- .config/nvim/lua/melxaco/plugins/nvim-tree.lua
+++ .config/nvim/lua/melxaco/plugins/nvim-tree.lua
@@ -8,6 +8,18 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup {}
+    require("nvim-tree").setup {
+      -- change folder arrow icons
+      renderer = {
+        icons = {
+          glyphs = {
+            folder = {
+              arrow_closed = "", -- arrow when folder is closed
+              arrow_open = "", -- arrow when folder is open
+            }
+          }
+        }
+      }
+    }
   end,
 }

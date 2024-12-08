e596b0d12945f37bc5b3afa70cc8f82256fa612d
Stijn Mertens <stijn.mertens@expertum.net>
2024-12-08 15:07:24 +0100

    feat: installed lualine + update telescope + install telescope-fzf-native for improved fuzy search

--- .config/nvim/lua/melxaco/plugins/telescope.lua
+++ .config/nvim/lua/melxaco/plugins/telescope.lua
@@ -1,5 +1,6 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+  "nvim-telescope/telescope-fzf-native.nvim", run = "make", -- dependency for better sorting performance0
+  "nvim-telescope/telescope.nvim", tag = "0.1.8",
 -- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    }
+  dependencies = { "nvim-lua/plenary.nvim" }
+}

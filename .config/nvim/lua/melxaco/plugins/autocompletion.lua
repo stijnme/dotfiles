cca81cbe4ad662be58777894c29962b252ae595c
Stijn Mertens <stijn.mertens@expertum.net>
2024-12-08 15:12:01 +0100

    feat: autocomplete plugins

---
+++ .config/nvim/lua/melxaco/plugins/autocompletion.lua
@@ -0,0 +1,6 @@
+return {
+  -- autocompletion
+  "hrsh7th/nvim-cmp",   -- completion plugin
+  "hrsh7th/cmp-buffer", -- source for text in buffer
+  "hrsh7th/cmp-path",   -- source for file system paths
+}

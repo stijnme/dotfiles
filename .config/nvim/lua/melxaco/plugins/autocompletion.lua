a6f79dc8197b0f5fc165cdb8ee03843ff6c0f43e
Stijn Mertens <stijn.mertens@expertum.net>
2024-12-08 15:30:56 +0100

    feat: added cmp-nvim-lsp

--- .config/nvim/lua/melxaco/plugins/autocompletion.lua
+++ .config/nvim/lua/melxaco/plugins/autocompletion.lua
@@ -3,4 +3,7 @@ return {
   "hrsh7th/nvim-cmp",   -- completion plugin
   "hrsh7th/cmp-buffer", -- source for text in buffer
   "hrsh7th/cmp-path",   -- source for file system paths
+
+  "hrsh7th/cmp-nvim-lsp", -- integrate with lsp for autocompletion
+  dependencies = { "neovim/nvim-lspconfig" }
 }

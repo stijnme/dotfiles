029b754d76a7579633b3a0864e32c79315e4a684
Stijn Mertens <stijn.mertens@expertum.net>
2024-12-08 15:27:01 +0100

    feat: installed mason + nvim-lspconfig

---
+++ .config/nvim/lua/melxaco/plugins/mason.lua
@@ -0,0 +1,10 @@
+return {
+  -- managing & installing lsp servers, linters & formatters
+  "williamboman/mason.nvim",           -- in charge of managing lsp servers, linters & formatters
+  "williamboman/mason-lspconfig.nvim", -- bridges gap b/w mason & lspconfig
+
+  -- easily configure language servers
+  "neovim/nvim-lspconfig",
+
+  -- configuring lsp servers
+}

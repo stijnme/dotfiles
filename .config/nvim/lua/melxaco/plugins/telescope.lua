0c5f6d471e1e290da7a3b386d5ac2b9917319c17
Stijn Mertens <stijn.mertens@expertum.net>
2025-03-31 21:58:40 +0200

    feat: configured telescope

--- .config/nvim/lua/melxaco/plugins/telescope.lua
+++ .config/nvim/lua/melxaco/plugins/telescope.lua
@@ -1,6 +1,32 @@
 return {
-  "nvim-telescope/telescope-fzf-native.nvim", run = "make", -- dependency for better sorting performance0
   "nvim-telescope/telescope.nvim", tag = "0.1.8",
 -- or                              , branch = '0.1.x',
-  dependencies = { "nvim-lua/plenary.nvim" }
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" }, -- dependency for better sorting performance0
+  },
+--  dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" },
+  config = function()
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+
+
+    telescope.setup({
+      defaults = {
+         prompt_prefix = "❯ ",
+         selection_caret = "❯ ",
+        mappings = {
+          i = {
+            -- map actions.which_key to <C-h> (default: <C-/>)
+            -- actions.which_key shows the mappings for your picker,
+            -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+            ["<C-h>"] = "which_key",
+            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+            ["<C-j>"] = actions.move_selection_next, -- move to next result
+            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
+          }
+        }
+      }
+    })
+  end,
 }

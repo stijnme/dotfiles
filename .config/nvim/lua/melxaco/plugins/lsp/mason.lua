09e388336440ee930835bb781a103043dfdda90d
Stijn Mertens <stijn.mertens@expertum.net>
2025-04-22 21:20:19 +0200

    chore: corrected indentation

--- .config/nvim/lua/melxaco/plugins/lsp/mason.lua
+++ .config/nvim/lua/melxaco/plugins/lsp/mason.lua
@@ -6,33 +6,33 @@ return {
   },
 
   config = function()
-  require("mason").setup({
-    ui = {
-      icons = {
-        package_installed = "✓",
-        package_pending = "➜",
-        package_uninstalled = "✗"
+    require("mason").setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
+        }
       }
-    }
-  })
+    })
 
-  require("mason-lspconfig").setup({
-    ensure_installed = {
-      "bashls",
---    "clangd",
---    "cmake",
-      "cds-lsp",
-      "cssls",
-      "eslint",
-      "html",
-      "jsonls",
-      "lua_ls",
-      "ts_ls",
-      "sqlls",
-      "yamlls" 
-    },
-    automatic_installation = true,
-  })
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "bashls",
+--      "clangd",
+--      "cmake",
+        "cds-lsp",
+        "cssls",
+        "eslint",
+        "html",
+        "jsonls",
+        "lua_ls",
+        "ts_ls",
+        "sqlls",
+        "yamlls"
+      },
+      automatic_installation = true,
+    })
 
 -- After setting up mason-lspconfig you may set up servers via lspconfig
 -- require("lspconfig").lua_ls.setup {}

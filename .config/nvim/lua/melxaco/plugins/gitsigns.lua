95d7de470e231471a897af9c795b501400361102
Stijn Mertens <stijn.mertens@expertum.net>
2025-11-29 21:36:17 +0100

    fix: updated typo in gitsigns config

--- .config/nvim/lua/melxaco/plugins/gitsigns.lua
+++ .config/nvim/lua/melxaco/plugins/gitsigns.lua
@@ -2,7 +2,7 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     opts = {
-      sign_column = true,
+      signcolumn = true,
       auto_attach = true,
       signs_staged_enable = true,
       current_line_blame = true,

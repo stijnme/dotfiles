8e45f4c71d2c07cf2c3b069df9b18bd339a4b729
Stijn Mertens <stijn.mertens@expertum.net>
2025-11-29 18:15:56 +0100

    feat: enabled lsp again + autocompletion from buffer and for files

--- .config/nvim/lua/melxaco/plugins/autocompletion.lua
+++ .config/nvim/lua/melxaco/plugins/autocompletion.lua
@@ -1,9 +1,32 @@
 return {
   -- autocompletion
-  "hrsh7th/nvim-cmp",   -- completion plugin
-  "hrsh7th/cmp-buffer", -- source for text in buffer
-  "hrsh7th/cmp-path",   -- source for file system paths
-
-  "hrsh7th/cmp-nvim-lsp", -- integrate with lsp for autocompletion
-  dependencies = { "neovim/nvim-lspconfig" }
+  "hrsh7th/nvim-cmp",   -- completion engine plugin
+  dependencies = {
+    "hrsh7th/cmp-buffer", -- source for text in buffer
+    "hrsh7th/cmp-path",   -- source for file system paths
+  },
+  config = function()
+    local cmp = require("cmp")
+    cmp.setup({
+      -- snippet = {
+      --   expand = function(args)
+      --     vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      --   end,
+      -- },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+        ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+        ["<C-e>"] = cmp.mapping.abort(), -- close completion window
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' }, -- add lsp as a source, see ../lsp/autocmp-nvim-lsp.lua
+        { name = 'buffer' },
+        { name = 'path' },
+      })
+    })
+  end,
 }
--- .config/nvim/lua/melxaco/plugins/lsp/mason.lua
+++ .config/nvim/lua/melxaco/plugins/lsp/mason.lua
@@ -1,42 +1,38 @@
+--print(os.time() .. " [D] mason config running")
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  "mason-org/mason-lspconfig.nvim",
+  opts = {
+    ensure_installed = {
+      "bashls",
+      "bashls",
+--    "clangd",
+--    "cmake",
+      "cds-lsp",
+      "cssls",
+      "eslint",
+      "html",
+      "jsonls",
+      "lua_ls",
+      "ts_ls",
+      "sqlls",
+      "yamlls"
+    },
   },
-
-  config = function()
-    require("mason").setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗"
+  dependencies = { -- following need to load before mason-lspconfig
+    {
+      "mason-org/mason.nvim",
+      opts = {
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+          }
         }
-      }
-    })
-
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "bashls",
---      "clangd",
---      "cmake",
-        "cds-lsp",
-        "cssls",
-        "eslint",
-        "html",
-        "jsonls",
-        "lua_ls",
-        "ts_ls",
-        "sqlls",
-        "yamlls"
       },
-      automatic_installation = true,
-    })
-
--- After setting up mason-lspconfig you may set up servers via lspconfig
--- require("lspconfig").lua_ls.setup {}
--- require("lspconfig").rust_analyzer.setup {}
--- ...
-  end,
+    },
+    {
+      "nvim-lspconfig", -- see other config file
+    }
+  },
 }

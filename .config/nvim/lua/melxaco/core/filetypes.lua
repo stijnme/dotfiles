7bd41630e2b5706c4f5e17d2cb812ff7e796a305
Stijn Mertens <stijn.mertens@expertum.net>
2025-12-02 21:44:40 +0100

    feat: - added filetype cds - added basis syntax highlighting for cds - added sample lps configuration

---
+++ .config/nvim/lua/melxaco/core/filetypes.lua
@@ -0,0 +1,6 @@
+-- Associate .cds files with a custom 'cds' filetype
+vim.filetype.add({
+  extension = {
+    cds = "cds",
+  }
+})
--- .config/nvim/lua/melxaco/init.lua
+++ .config/nvim/lua/melxaco/init.lua
@@ -1,6 +1,9 @@
 -- Config vim
+require("melxaco.core.filetypes")
 require("melxaco.core.options")
 require("melxaco.core.keymaps")
 
 -- Configure Lazy
 require("melxaco.config.lazy")
+-- Configure LSP
+require("melxaco.config.lsp")

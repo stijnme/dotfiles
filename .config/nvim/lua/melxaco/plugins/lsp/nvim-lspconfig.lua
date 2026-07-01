ebc0110c41b9b371e8ef2bc3c9743164a33b82d6
Stijn Mertens <stijn.mertens@expertum.net>
2026-07-01 09:57:31 +0200

    fix: added global vim for neovim plugin development

--- .config/nvim/lua/melxaco/plugins/lsp/nvim-lspconfig.lua
+++ .config/nvim/lua/melxaco/plugins/lsp/nvim-lspconfig.lua
@@ -1,3 +1,10 @@
 return {
   "neovim/nvim-lspconfig",
+  settings = {
+    Lua = { -- avoid "undefined global" diagnostic error for vim global when using lua_ls
+      diagnostics = {
+        globals = { "vim" },
+      },
+    },
+  },
 }
